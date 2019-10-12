77e1ddc27d7b433b781b2d9dfa3a22c418ec6e09
issamelferkh <issam.elferkh@gmail.com>
2019-10-12 21:46:23 +0100

    Add Day02 ex00, ex01

--- Day02/ex00/another_world.php
+++ Day02/ex00/another_world.php
@@ -1,5 +1,13 @@
 #!/usr/bin/php
 <?php
-    if ($argc < 2)
-        exit();
-    echo trim(preg_replace("/[ \t\r]+/", " ", $argv[1]))."\n";
+if ($argc < 2)
+    exit();
+//echo trim(preg_replace("/[ \t\r]+/", " ", $argv[1]))."\n";
+
+// $str = trim($argv[1]);
+// echo ($str."\n");
+// $str = preg_replace("/\s+/", " ", $str);
+// echo ($str."\n");
+
+echo (trim(preg_replace("/\s+/", " ", $argv[1]))."\n");
+?>
--- Day02/ex01/one_more_time.php
+++ Day02/ex01/one_more_time.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php
-    date_default_timezone_set('Europe/Paris');
+    date_default_timezone_set('Africa/Casablanca');
 
     $month = array(
         1 => "janvier",
@@ -18,7 +18,7 @@
 
     $week = array(
         1 => "lundi",
-        2 => "mardi",
+        2 => "mardi", 
         3 => "mercredi",
         4 => "jeudi",
         5 => "vendredi",
@@ -29,20 +29,24 @@
         exit();
 
     $date = explode(" ", $argv[1]);
+    //foreach ($date as $d)
+    //    echo ("Date ===> ".$d."\n"); //echo all elements of Date
+    //echo ("Count Date ---> ".count($date)."\n"); // for test nbr of elements in Date -> 5 elements
+    /* Verif numbers*/
     if (count($date) != 5 ||
-        preg_match("/^[1-9]$|0[1-9]|[1-2][0-9]|3[0-1]$/", $date[1], $date[1]) === 0 ||
+        preg_match("/^[1-9]$|[1-2]0|[1-2][0-9]|3[0-1]$/", $date[1], $date[1]) === 0 ||
         preg_match("/^[0-9]{4}$/", $date[3], $date[3]) === 0 ||
         preg_match("/^([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/", $date[4], $date[4]) === 0) {
         echo "Wrong Format\n";
         exit();
     }
+    /* Verif Day and Month */
     $date[0] = array_search(lcfirst($date[0]), $week);
     $date[2] = array_search(lcfirst($date[2]), $month);
     if ($date[0] === false || $date[2] === false){
         echo "Wrong Format\n";
         exit();
     }
-
     $time = mktime($date[4][1], $date[4][2], $date[4][3], $date[2], $date[1][0], $date[3][0]);
     if (date( "N", $time) == $date[0])
         echo $time."\n";
